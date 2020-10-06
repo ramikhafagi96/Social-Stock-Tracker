36f4bf73434ae470f0b52d81684fefdada869a54
ramikh96 <ramikh.developer@gmail.com>
2020-10-06 13:45:20 +0200

    styling the devis views

--- Gemfile
+++ Gemfile
@@ -19,6 +19,10 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 # Add devise gem
 gem 'devise'
+# Add devis-bootstrap gem
+gem 'devise-bootstrap-views'
+# Add iex-ruby-client
+gem 'iex-ruby-client'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_182513) do
+ActiveRecord::Schema.define(version: 2020_10_05_205906) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
