d995e744d5103b59b45e5a9386223e9087bbf572
ramikh96 <ramikh.developer@gmail.com>
2020-10-13 13:20:49 +0200

    untrack a stock feature

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -10,5 +10,14 @@ class UserStocksController < ApplicationController
         flash[:notice] = "Stock object #{stock.name} is added to your portfolio"
         redirect_to my_portfolio_path
     end
+
+    def destroy
+        stock = Stock.find(params[:id])
+        user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+        user_stock.destroy
+        flash[:notice] = "#{stock.ticker} was successfully removed from portfolio"
+        redirect_to my_portfolio_path
+    end
+    
     
 end
