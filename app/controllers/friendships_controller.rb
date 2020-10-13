1ba9f8c1a88fd840d2a8148843011034083397d9
ramikh96 <ramikh.developer@gmail.com>
2020-10-13 16:57:46 +0200

    implemented unfollow friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,15 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Friend Followed"
+    
+    else
+      flash[:alert] = "Something Went Wrong"
+    end
+
+    redirect_to my_friends_path
   end
 
   def destroy
